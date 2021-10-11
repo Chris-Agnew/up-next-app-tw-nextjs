004f4e6b9125fd4f44509fa1e4852a3a7f8aaf52
Chris <chris@chris-agnew.com>
2021-10-11 15:56:34 -0400

    add navbar

---
+++ components/Navbar.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="w-screen h-32 bg-black text-gray-200 flex">
+      <div className="flex justify-center items-center w-1/3">
+        <Image src="/images/up-next-logo.png" height={250} width={250} />
+      </div>
+      <div className="flex justify-center items-center w-1/3">
+        <Link href="/new">
+          <h3 className="cursor-pointer">New Episodes</h3>
+        </Link>
+        <Link href="/popular">
+          <h3 className="px-5 cursor-pointer">Popular Episodes</h3>
+        </Link>
+        <Link href="/coming">
+          <h3 className="cursor-pointer">Coming Soon</h3>
+        </Link>
+      </div>
+      <div className="flex justify-center items-center w-1/3"></div>
+    </div>
+  );
+};
+
+export default Navbar;
