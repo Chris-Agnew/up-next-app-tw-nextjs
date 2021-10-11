27d457b23fabf0df516de99aeca913eaf5126456
Chris <chris@chris-agnew.com>
2021-10-11 15:57:01 -0400

    add firebase logic

---
+++ components/LoggedIn.js
@@ -0,0 +1,17 @@
+// import { useEffect } from "react";
+// import { useRouter } from "next/router";
+// import { useAuth } from "../firebase/AuthContext";
+
+// const LoggedIn = () => {
+//   //   const { authUser, loading } = useAuth();
+//   const router = useRouter();
+
+//   // Listen for changes on loading and authUser, redirect if needed
+//   useEffect(() => {
+//     if (!loading && !authUser) router.push("/");
+//   }, [authUser, loading]);
+
+//   return null;
+// };
+
+// export default LoggedIn;
