ebc20ba31df3095a02b936ccb8e139593e0acce8
Chris <chris@chris-agnew.com>
2021-10-13 17:28:42 -0400

    add hover effects to nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,22 +8,27 @@ const Navbar = () => {
         <Image src="/images/up-next-logo.png" height={250} width={250} />
       </div>
       <div className="flex justify-center items-center w-1/3">
-        <Link href="/new">
-          <h3 className="cursor-pointer">New Episodes</h3>
+        <Link href="#">
+          <h3 className="cursor-pointer hover:text-blue-600">New Episodes</h3>
         </Link>
-        <Link href="/popular">
-          <h3 className="px-5 cursor-pointer">Popular Episodes</h3>
+        <Link href="#">
+          <h3 className="px-5 cursor-pointer hover:text-blue-600">
+            Popular Episodes
+          </h3>
         </Link>
-        <Link href="/coming">
-          <h3 className="cursor-pointer">Coming Soon</h3>
+        <Link href="#">
+          <h3 className="cursor-pointer hover:text-blue-600">Coming Soon</h3>
         </Link>
       </div>
       <div className="flex justify-center items-center w-1/3">
         <Link href="/auth">
-          <h3 className="cursor-pointer">Sign In</h3>
+          <h3 className="cursor-pointer hover:text-blue-600">Sign In</h3>
         </Link>
         <Link href="/auth">
-          <button type="submit" className="p-3 mx-5 bg-blue-600 rounded-lg">
+          <button
+            type="submit"
+            className="p-3 mx-5 bg-blue-600 hover:bg-blue-900 rounded-lg"
+          >
             Sign Up
           </button>
         </Link>
