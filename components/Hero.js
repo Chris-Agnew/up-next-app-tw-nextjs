9d17e6360d9ea302836caf46e20fec3d68fb17cf
Chris <chris@chris-agnew.com>
2021-10-14 13:53:28 -0400

    fix image sizing

--- components/Hero.js
+++ components/Hero.js
@@ -6,24 +6,37 @@ const Hero = ({ results }) => {
   const tvShow = useRef(null);
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-    <div className="">
+    <div className="px-2 bg-black">
       <Rerousel itemRef={tvShow}>
         {results.map(result => {
           return (
             <div className="relative text-gray-200" key={result.id}>
               <div className="bg-black ">
-                <img
+                <Image
                   src={
                     `${BASE_URL}${
                       result.backdrop_path || result.poster_path
                     }` || `${BASE_URL}${result.poster_path}`
                   }
-                  height={1080}
+                  height={1000}
                   width={1920}
                   ref={tvShow}
                   alt={result.name}
                   key={result.id}
-                  className="opacity-80"
+                  className="opacity-80 hidden md:flex"
+                />
+                <Image
+                  src={
+                    `${BASE_URL}${
+                      result.backdrop_path || result.poster_path
+                    }` || `${BASE_URL}${result.poster_path}`
+                  }
+                  height={500}
+                  width={400}
+                  ref={tvShow}
+                  alt={result.name}
+                  key={result.id}
+                  className="opacity-80 flex md:hidden"
                 />
               </div>
 
